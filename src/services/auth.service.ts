081a39ff010cb854b681abd52931e5a565eb25ca
akim1995 <akim.khalitov.ya@gmail.com>
2019-05-15 19:07:33 +0200

    services

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,68 +1,70 @@
-import { UserManager, WebStorageStateStore, User, UserManagerSettings } from 'oidc-client'
-
-import axios from 'axios'
+import { UserManager, WebStorageStateStore, User, UserManagerSettings } from "oidc-client";
+import axios from "axios";
+import cookieStorage from "cookie-storage-domain";
 
 type UserRegistration = {
-  login: string;
-  password: string;
-}
+    login: string;
+    password: string;
+};
 
 export class AuthService {
-  private userManager: UserManager;
-  private identityServer = process.env.VUE_APP_IDENTITY_SERVER;
-  private identityServerApi = process.env.VUE_APP_IDENTITY_SERVER_API;
+    public userManager: UserManager;
+    private identityServer = process.env.VUE_APP_IDENTITY_SERVER;
+    private identityServerApi = process.env.VUE_APP_IDENTITY_SERVER_API;
+    private webAddress = process.env.VUE_APP_WEB_APP;
 
-  constructor () {
-    const AUTH0_DOMAIN = this.identityServer
-    const MY_HOST: string = window.location.origin
+    constructor() {
+        const AUTH0_DOMAIN = this.identityServer;
+        //const MY_HOST: string = window.location.origin;
 
-    const settings: UserManagerSettings = {
-      userStore: new WebStorageStateStore({ store: window.localStorage }),
-      authority: AUTH0_DOMAIN,
-      client_id: 'kl',
-      redirect_uri: `${MY_HOST}/new/callback.html`,
-      post_logout_redirect_uri: `${MY_HOST}/`,
-      response_type: 'id_token token',
-      scope: 'openid profile roles',
-      filterProtocolClaims: true,
-      loadUserInfo: true,
-      metadata: {
-        issuer: 'identity',
-        authorization_endpoint: AUTH0_DOMAIN + '/connect/authorize',
-        userinfo_endpoint: AUTH0_DOMAIN + '/connect/userinfo',
-        end_session_endpoint: AUTH0_DOMAIN + '/connect/endsession',
-        jwks_uri: AUTH0_DOMAIN + '/.well-known/openid-configuration/jwks'
-      }
+        const settings: UserManagerSettings = {
+            userStore: new WebStorageStateStore({ store: cookieStorage }),
+            stateStore: new WebStorageStateStore({ store: cookieStorage }),
+            authority: AUTH0_DOMAIN,
+            client_id: "kl",
+            redirect_uri: `${this.webAddress}/callback/signin-oidc`,
+            post_logout_redirect_uri: `${this.webAddress}/callback/signout-oidc`,
+            response_type: "id_token token",
+            scope: "openid profile roles",
+            filterProtocolClaims: true,
+            loadUserInfo: true,
+            metadata: {
+                issuer: "identity",
+                authorization_endpoint: AUTH0_DOMAIN + "/connect/authorize",
+                userinfo_endpoint: AUTH0_DOMAIN + "/connect/userinfo",
+                end_session_endpoint: AUTH0_DOMAIN + "/connect/endsession",
+                jwks_uri: AUTH0_DOMAIN + "/.well-known/openid-configuration/jwks",
+            },
+        };
+
+        this.userManager = new UserManager(settings);
     }
 
-    this.userManager = new UserManager(settings)
-  }
+    public registration(user: UserRegistration) {
+        return axios.post<boolean>(`${this.identityServerApi}/api/account/register`, user);
+    }
 
-  public registration (user: UserRegistration) {
-    return axios.post<boolean>(`${this.identityServerApi}/api/account/register`, user)
-  }
+    public getUser(): Promise<User> {
+        return this.userManager.getUser();
+    }
 
-  public getUser (): Promise<User> {
-    return this.userManager.getUser()
-  }
+    public login(): Promise<void> {
+        return this.userManager.signinRedirect();
+    }
 
-  public login (): Promise<void> {
-    return this.userManager.signinRedirect()
-  }
+    public logout(): Promise<void> {
+        return this.userManager.signoutRedirect();
+    }
 
-  public logout (): Promise<void> {
-    return this.userManager.signoutRedirect()
-  }
+    public saveUserInfo(key: string, user: User): void {
+        cookieStorage.setItem(key, JSON.stringify(user));
+    }
 
-  public saveUserInfo (key: string, user: User): void {
-    localStorage.setItem(key, JSON.stringify(user))
-  }
+    public removeFromCookieStorageByKey(key: string): void {
+        cookieStorage.removeItem(key);
+    }
 
-  public removeFromLocalStorageByKey (key: string): void {
-    localStorage.removeItem(key)
-  }
-
-  public updateUserStorage (key:string, userObject: User): void {
-    this.userManager.settings.userStore.set(key, JSON.stringify(userObject))
-  }
+    public updateUserStorage(key: string, userObject: User): void {
+        this.userManager.settings.userStore.set(key, JSON.stringify(userObject));
+    }
 }
--- src/services/profile.service.ts
+++ src/services/profile.service.ts
@@ -1,35 +1,37 @@
-import axios from 'axios'
-import { IUserProfile } from '@/models/IUserProfile'
-import { User } from 'oidc-client'
-import { store } from '@/store/index'
+import axios from "axios";
+import { IUserProfile } from "@/models/IUserProfile";
+import { User } from "oidc-client";
+import cookieStorage from "cookie-storage-domain";
+import Vue from "vue";
+// import { store } from "@/store/index";
 
-export class ProfileService {
+export class ProfileService extends Vue {
     private webAppHost = process.env.VUE_APP_WEB_APP;
     private identityServerHost = process.env.VUE_APP_IDENTITY_SERVER;
-    private userKey = 'user';
+    private userKey = "user";
     private identityUserKey = `${this.userKey}:${this.identityServerHost}:kl`;
 
-    public getProfileInfoByUserId (login : string) {
-      return axios.get<IUserProfile>(`${this.webAppHost}/api/profile/${login}`)
-        .then(x => x)
+    public getProfileInfoByUserId(login: string) {
+        return axios.get<IUserProfile>(`${this.webAppHost}/api/profile/${login}`).then(x => x);
     }
 
-    public updateProfileInfo (updatedUserProfile: IUserProfile) {
-      return axios.put<boolean>(`${this.webAppHost}/api/profile`, updatedUserProfile)
-        .then(x => {
-          this.updateUserName(updatedUserProfile.name)
-          return x
-        })
+    public updateProfileInfo(updatedUserProfile: IUserProfile) {
+        return axios.put<boolean>(`${this.webAppHost}/api/profile`, updatedUserProfile).then(x => {
+            this.updateUserName(updatedUserProfile.name);
+            return x;
+        });
     }
 
-    private updateUserName (newUserName: string) {
-      const key = this.identityUserKey
-      const user = localStorage.getItem(this.userKey)
-      if (!user) { return }
-      const userObject = JSON.parse(user) as User
-      userObject.profile.name = newUserName
-      userObject.profile.preferred_username = newUserName
-      localStorage.setItem(this.userKey, JSON.stringify(userObject))
-      store.dispatch('auth/updateUser', { key, userObject })
+    private updateUserName(newUserName: string) {
+        const key = this.identityUserKey;
+        const user = cookieStorage.getItem(this.userKey);
+        if (!user) {
+            return;
+        }
+        const userObject = JSON.parse(user) as User;
+        userObject.profile.name = newUserName;
+        userObject.profile.preferred_username = newUserName;
+        cookieStorage.setItem(this.userKey, JSON.stringify(userObject));
+        this.$store.dispatch("auth/updateUser", { key, userObject });
     }
 }
